8b663f98eb987eecd8b0759dd21e92473f799ae4
Zachary Collier <zacharycollier36@protonmail.com>
2025-08-15 17:40:30 -0230

    Hardcoded API endpoint - we can see about un-hardcoding it later.

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,7 @@
-import axios from 'axios'
-const base = import.meta.env.VITE_API_URL || '/api'
-const api = axios.create({ baseURL: base, headers: { 'Content-Type': 'application/json' } })
-export default api
+import axios from "axios";
+const base = `http://18.117.89.174:8080` || "/";
+const api = axios.create({
+	baseURL: base,
+	headers: { "Content-Type": "application/json" },
+});
+export default api;
